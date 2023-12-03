52159e3caa4a5117d5b6f70268a5117b8205cf9a
Geert van Ieperen <ieperen3039@outlook.com>
2023-12-03 20:47:28 +0100

    part one

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+#[allow(dead_code)]
+pub mod trebuchet;
+
+use std::path::PathBuf;
+
+use clap::Parser;
+
+/// All exercises for advent of code 2023
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CommandlineArguments {
+    /// which puzzle to solve
+    #[arg(short, long)]
+    day: i32,
+    /// input file
+    #[arg(short, long)]
+    input: PathBuf,
+    /// output file
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+}
+
+fn main() {
+    // let args = CommandlineArguments::parse();
+    let args = CommandlineArguments {
+        day: 1,
+        input: PathBuf::from("inputs/trebuchet.txt"),
+        output: None,
+    };
+
+    let input_text = {
+        let file_contents = std::fs::read_to_string(&args.input);
+        if file_contents.is_err() {
+            panic!("File {} can not be opened", args.input.display())
+        } else {
+            file_contents.unwrap()
+        }
+    };
+
+    let output_file = args.output.map(|output|{
+        let file_contents = std::fs::File::create(&output);
+        if file_contents.is_err() {
+            panic!("File {} can not be opened", output.display())
+        } else {
+            file_contents.unwrap()
+        }
+    });
+
+    match args.day {
+        1 => {
+            let value = trebuchet::decode_calibration(input_text);
+            println!("{value}");
+        },
+        _ => panic!("day {} not defined", args.day)
+    }
+
+}
---
+++ src/trebuchet.rs
@@ -0,0 +1,53 @@
+const NUMBERS: &[&str] = &[
+    "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+];
+
+pub fn decode_calibration(input: String) -> u32 {
+    let split = input.split("\n");
+    let mut total = 0;
+
+    for line in split {
+        let line_str = line.to_string().clone();
+
+        let mut first = 0;
+        for char_idx in 0..line.len() {
+            let line_progress = &line[char_idx..];
+
+            if let Some(num) = check_for_number(line_progress) {
+                first = num;
+                break;
+            }
+        }
+
+        let mut second = 0;
+        for char_idx in (0..line.len()).rev() {
+            let line_progress = &line[char_idx..];
+
+            if let Some(num) = check_for_number(line_progress) {
+                second = num;
+                break;
+            }
+        }
+        println!("{first}, {second} : {line_str}");
+
+        total += (first * 10) + second;
+    }
+
+    total
+}
+
+fn check_for_number(line_progress: &str) -> Option<u32> {
+    let first_char = line_progress.chars().nth(0).unwrap();
+    if first_char.is_numeric() {
+        return Some(first_char.to_digit(10).unwrap());
+    } else {
+        // start at one, we skip "zero"
+        for num_idx in 1..=9 {
+            if line_progress.starts_with(NUMBERS[num_idx]) {
+                return Some(num_idx as u32);
+            }
+        }
+    }
+
+    return None;
+}
